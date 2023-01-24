1b230e1eaccf00008eead3929311dc278e127733
mygitgor <mypywelcome@gmail.com>
2023-01-24 16:13:16 +0400

    aded md

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿string[] strings = { "hello", "world", "2", "(.v.)", "12", "\0" }; 
+var result = strings.Where(s => s.Length <= 3).ToArray(); 
+Console.WriteLine(string.Join(", ", result)); 
+Console.ReadLine();
